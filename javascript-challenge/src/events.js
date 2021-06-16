201385381394ccf93e760e7c70f9628794ecbfe5
Rinon <rinon.ten@onja.org>
2021-06-16 14:51:23 +0300

    Optimizing codes, checking if the functions received the right arguments

--- javascript-challenge/src/events.js
+++ javascript-challenge/src/events.js
@@ -66,8 +66,6 @@ const arrayOfEvents = [
  Your solution should not modify any of the function arguments
 */
 
-// Sorting the array of events
-
 const sortArrayOfEvents = (events) => {
   return events.sort(
     (firstEvent, secondEvent) =>
@@ -76,41 +74,43 @@ const sortArrayOfEvents = (events) => {
 };
 
 const groupEventsByDay = (events) => {
-  const arrayOfEventsSorted = sortArrayOfEvents(events);
-  const firstEventObj = arrayOfEventsSorted[0];
-  // convert first date
-  const momentOfFirstDate = moment(firstEventObj.startsAt);
+  if (events && typeof events === 'object' && events !== []) {
+    const arrayOfEventsSorted = sortArrayOfEvents(events);
+    const firstEventObj = arrayOfEventsSorted[0];
 
-  // Add a difference property to each event
-  const eventsArrayWithDifference = events.map((event) => {
-    const date = moment(event.startsAt);
-    const dateDifference = date.diff(momentOfFirstDate, 'days');
-    return {
-      ...event,
-      difference: dateDifference,
-    };
-  });
+    // convert first date
+    const momentOfFirstDate = moment(firstEventObj.startsAt);
 
-  // Grouping events according to the day differences
-  events = eventsArrayWithDifference.reduce(function (allEvents, event) {
-    allEvents[event.difference] = allEvents[event.difference] || [];
-    allEvents[event.difference].push(event);
-    return allEvents;
-  }, {});
-
-  // Mapping through the key values and remove the difference key value from the event
-  Object.keys(events).map((key) => {
-    events[key] = events[key].map((event) => {
-      return {
-        id: event.id,
-        startsAt: event.startsAt,
-        endsAt: event.endsAt,
-        title: event.title,
+    // Adding a difference property to each event and grouping events according to the day differences
+    const eventsGroupedByDay = events.reduce((allEvents, event) => {
+      const date = moment(event.startsAt);
+      const dateDifference = date.diff(momentOfFirstDate, 'days');
+      event = {
+        ...event,
+        difference: dateDifference,
       };
-    });
-  });
 
-  return events;
+      allEvents[event.difference] = allEvents[event.difference] || [];
+      allEvents[event.difference].push(event);
+      return allEvents;
+    }, {});
+
+    events = eventsGroupedByDay;
+    // Restore the event's object structure to its original structure
+    Object.keys(events).map((key) => {
+      events[key] = events[key].map((event) => {
+        return {
+          id: event.id,
+          startsAt: event.startsAt,
+          endsAt: event.endsAt,
+          title: event.title,
+        };
+      });
+    });
+    return events;
+  } else {
+    return `Invalid input`;
+  }
 };
 
 groupEventsByDay(arrayOfEvents);
@@ -151,64 +151,64 @@ groupEventsByDay(arrayOfEvents);
 */
 
 const moveEventToDay = (eventsByDay, id, toDay) => {
-  if (
-    typeof eventsByDay === 'object' &&
-    typeof id === 'number' &&
-    id >= 0 &&
-    typeof toDay === 'number' &&
-    toDay >= 0
-  ) {
-    const firstEvent = eventsByDay[0][0];
+  if (eventsByDay && id && toDay) {
+    if (
+      typeof eventsByDay === 'object' &&
+      typeof id === 'number' &&
+      id >= 0 &&
+      typeof toDay === 'number' &&
+      toDay >= 0
+    ) {
+      const firstEvent = eventsByDay[0][0];
+      Object.keys(eventsByDay).map((key) => {
+        const eventsFromKeys = eventsByDay[key];
+        const targetedEvent = eventsFromKeys.filter((event) => event.id === id);
 
-    Object.keys(eventsByDay).map((key) => {
-      const eventsFromKeys = eventsByDay[key];
-      const targetedEvent = eventsFromKeys.filter((event) => event.id === id);
+        // Change dates of the targeted event
+        const targetedEventUpdated = targetedEvent.map((event) => {
+          const eventStartsAt = JSON.stringify(
+            addDays(parseISO(firstEvent.startsAt), toDay),
+          );
 
-      // Find dates of the targeted event
-      const targetedEventUpdated = targetedEvent.map((event) => {
-        const eventStartsAt = JSON.stringify(
-          addDays(parseISO(firstEvent.startsAt), toDay),
+          const eventEndsAt = JSON.stringify(
+            addDays(parseISO(firstEvent.endsAt), toDay),
+          );
+          return {
+            ...event,
+            startsAt: eventStartsAt.replace(/["]/g, ''),
+            endsAt: eventEndsAt.replace(/["]/g, ''),
+          };
+        });
+
+        const eventsWithoutTheTargetedOne = eventsByDay[key].filter(
+          (event) => event.id !== id,
         );
-        const eventEndsAt = JSON.stringify(
-          addDays(parseISO(firstEvent.endsAt), toDay),
-        );
 
-        return {
-          ...event,
-          startsAt: eventStartsAt.replace(/[ "]/g, ''),
-          endsAt: eventEndsAt.replace(/[ "]/g, ''),
-        };
-      });
+        eventsByDay[key] = [...eventsWithoutTheTargetedOne];
 
-      const eventsWithoutTheTargetedOne = eventsByDay[key].filter(
-        (event) => event.id !== id,
-      );
-      eventsByDay[key] = [...eventsWithoutTheTargetedOne];
-
-      if (eventsByDay[toDay]) {
-        const removeUndefinedEvent = (eventsArr) => {
-          return eventsArr.filter((event) => event !== undefined);
-        };
-
-        const eventMovedToDay = removeUndefinedEvent(eventsByDay[toDay]);
-
-        if (eventMovedToDay !== []) {
-          const eventMovedToDayWithPreviousEvent = [
+        if (eventsByDay[toDay] && eventsByDay[toDay] !== []) {
+          const eventMovedToDay = eventsByDay[toDay];
+          const eventMovedToDayWithPreviousEvents = [
             ...eventMovedToDay,
             targetedEventUpdated[0],
           ];
 
-          eventsByDay[toDay] = removeUndefinedEvent(
-            eventMovedToDayWithPreviousEvent,
+          // Remove any undefined event if there is in the events
+          const updatedMovedToDayEvent = eventMovedToDayWithPreviousEvents.filter(
+            (event) => event !== undefined,
           );
+
+          eventsByDay[toDay] = updatedMovedToDayEvent;
+        } else {
+          eventsByDay[toDay] = [targetedEventUpdated[0]]; // It can be undefined
         }
-      } else {
-        eventsByDay[toDay] = [targetedEventUpdated[0]];
-      }
-    });
-    return eventsByDay;
+      });
+      return eventsByDay;
+    } else {
+      return `Invalid input! Verify your inputs: eventsByDay must be an object, id and toDay must be numbers and positive`;
+    }
   } else {
-    return `Invalid input! Verify your inputs: eventsByDay must be an object, id and toDay must be numbers and positive`;
+    return `Missing one or more arguments`;
   }
 };
 
